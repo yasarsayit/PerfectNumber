48321b487e11c66f65d79e45aac79dcf2f78cd01
yasarsayit <90604486+yasarsayit@users.noreply.github.com>
2023-05-12 15:39:15 +0300

    Add files via upload

--- Exercise.java
+++ Exercise.java
@@ -1,5 +1,24 @@
 package java101;
 
-public class Exercise {
+import java.util.Scanner;
 
+public class Exercise {
+	public class Odev28MukemmelSayi {
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			int sayi, toplam = 0;
+			System.out.println("Sayı giriniz:");
+			sayi = input.nextInt();
+			for (int i = 1; i < sayi; i++) {
+				if (sayi % i == 0) {
+					toplam = toplam + i;
+				}
+			}
+			if (toplam == sayi) {
+				System.out.println(sayi + " Mükemmel sayıdır.");
+			} else {
+				System.out.println(sayi + " Mükemmel sayı değildir.");
+			}
+		}
+	}
 }
